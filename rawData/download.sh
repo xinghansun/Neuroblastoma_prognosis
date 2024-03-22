46eeca5efd35afa8e32c9cbfe7697875f23d9025
xinghansun <xinghansun@gmail.com>
2024-03-22 14:20:36 -0400

    Kmeans analysis to identify the subclusters

---
+++ rawData/download.sh
@@ -0,0 +1,5 @@
+# download neuroblastoma RNA-seq log2RPM at RefSeqId level from GSE62564
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE62nnn/GSE62564/suppl/GSE62564%5FSEQC%5FNB%5FRNA%2DSeq%5Flog2RPM.txt.gz
+
+# at gene level
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE49nnn/GSE49711/suppl/GSE49711%5FSEQC%5FNB%5FTAV%5FG%5Flog2.final.txt.gz
